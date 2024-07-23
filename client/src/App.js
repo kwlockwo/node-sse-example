21a185fc49462114e59faa08f8e2b3ef2dd9a50b
Keith Lockwood <kwlockwo@gmail.com>
2024-07-23 11:32:21 +1000

    init

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import ShortUniqueId from 'short-unique-id';
+import './App.css';
+
+const uid = new ShortUniqueId({ length: 5 });
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
+function App() {
+  const [ facts, setFacts ] = useState([]);
+  const [ listening, setListening ] = useState(false);
+
+  useEffect( () => {
+    if (!listening) {
+      const events = new EventSource(API_URL + '/events');
+
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+
+        setFacts((facts) => facts.concat(parsedData));
+      };
+
+      setListening(true);
+    }
+  }, [listening, facts]);
+
+  return (
+    <table className="stats-table">
+      <thead>
+        <tr>
+          <th>Fact</th>
+          <th>Source</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          facts.map((fact, i) =>
+            <tr key={uid.rnd()}>
+              <td>{fact.info}</td>
+              <td>{fact.source}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
